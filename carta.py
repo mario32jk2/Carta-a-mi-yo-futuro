ded13dc6ad56b330dd184055c87564c16b306bbc
Mario Castaño <64328554+mario32jk2@users.noreply.github.com>
2022-10-15 01:26:51 -0500

    Add files via upload

---
+++ carta.py
@@ -0,0 +1,11 @@
+#Carta a mi yo futuro
+print("fecha: 15/10/2022")
+
+print("Siento que elegi la carrera correcta, la voy a disfrutar si van haber desafios pero nada que con esfuerzo y dedicacion se pueda lograr, asi que en este momento tengo mi rumbo fijo ")
+
+print("En tres mese tengo que reforzar y tener dominado lo basico en python, y empezar hacer mis propias funciones y proyectos" )
+
+print("Si, es verdad que es un poco dificil pero acuerdate lo que te decias sobre la constancia y querer superar tus metas asi que por favor no dejes de programar, y todos los dias repasa para que seas el mejor del munfo")
+
+print("📈")
+
